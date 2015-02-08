f4e4dbeea5a2fa9f5503e87cecb6dc3a5c11454f
Dave Johnston <johnsto@gmail.com>
2015-02-08 16:03:35 +0000

    Improve installation instructions

--- README.md
+++ README.md
@@ -2,19 +2,35 @@
 
 *A tool to convert scanned documents into searchable PDFs*
 
-`ocrpdf` recognises and extracts text from scanned documents and combines them to produce (generally) searchable PDFs that look like the original document. It uses the open source [Tesseract](https://tesseract-ocr.googlecode.com) library for OCR, [Leptonica](http://leptonica.com) for image manipiulation, and [gofpdf](code.google.com/p/gofpdf) for document generation.
+`ocrpdf` recognises and extracts text from scanned documents then combines them into (generally) searchable PDFs that look like the original document. It uses the open source [Tesseract](https://tesseract-ocr.googlecode.com) library for OCR, [Leptonica](http://leptonica.com) for image manipiulation, [gofpdf](code.google.com/p/gofpdf) for document generation, and [kingpin](https://github.com/alecthomas/kingpin) for CLI support.
 
 ## I demand a GUI!
 
-You're probably better off with [`gscan2pdf`](http://gscan2pdf.sourceforge.net/), which was the original inspiration for this tool (obligatory Sourceforge warning!)
+You're probably better off with [`gscan2pdf`](http://gscan2pdf.sourceforge.net/), which was the original inspiration for this tool (obligatory Sourceforge warning)
 
 ## Installation
 
-This software requires that you have the Tesseract 3.03.03 library and data files installed.
+Use `go install bitbucket.org/johnsto/ocrpdf` to install `ocrpdf`
 
-On Debian systems, use `apt-get install libtesseract3 tesseract-ocr` to install the library and all language data files.
+Both the Leptonica and Tesseract libraries must be installed.
 
-On Windows, erm...
+### Leptonica
+
+Ensure that you have the Leptonica 1.71 library installed.
+
+* Debian: `apt-get install liblept3` (Jessie or newer)
+* Fedora: `yum install leptonica`
+* Arch: `pacman -S leptonica`
+* Windows: uh...
+
+### Tesseract
+
+Ensure that you have the Tesseract 3.03.03 library and data files installed.
+
+* Debian: `apt-get install libtesseract3 tesseract-ocr` (Jessie or newer)
+* Fedora: `yum install tesseract`
+* Arch `pacman -S tesseract tesseract-data-eng`
+* Windows: er...
 
 ## Usage
 
@@ -24,11 +40,19 @@ Converting a scanned image is as simple as:
 
 By default, `ocrpdf` will take the filename name of the first input scan as the output document name, in this case, `scan.pdf`.
 
-You can also specify a document size, output filename, title, enable compression and specify multiple pages:
+You can also specify a document size, output filename, document title, enable compression and specify multiple pages:
 
-`ocrpdf -s letter -o taxes.pdf -t "2015 Taxes" --compress taxes1.jpg taxes2.jpg taxes3.jpg`
+    ocrpdf -s letter \
+           -o taxes.pdf \
+           -t "2015 Taxes" \
+           --compress \
+           taxes1.jpg taxes2.jpg taxes3.jpg
 
 Automatic contrast enhancement to improve the legibility of the text is performed by default, you can disable this with the `--contrast=0` flag.
+
+## Image support
+
+Reading JPEGs, PNGs and TIFs is supported, plus any other formats that Leptonica supports. Images in outputted PDF will be either JPEG or PNG, based on the input format of the respective image. You can force a specific output format using the `--format` parameter.
 
 ## PDF Structure
 
--- ocrpdf.go
+++ ocrpdf.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -18,43 +17,35 @@ var (
 
 	app = kingpin.New("ocrpdf", "Converts scanned documents into searchable PDFs")
 
-	output = app.Flag("output", "output file name").Short('o').String()
+	input  = app.Arg("input", "input image filename(s)").Required().Strings()
+	output = app.Flag("output", "output filename").Short('o').String()
+	force  = app.Flag("force", "overwrite output file").Short('f').Bool()
 
 	tessData = app.Flag("tess-data", "Tesseract data directory").String()
 	tessLang = app.Flag("tess-lang", "Tesseract language").String()
 
-	docSize = app.Flag("size", "document size").
-		Short('s').Default("a4").String()
-	docTitle    = app.Flag("title", "document title").Short('t').String()
-	docKeywords = app.Flag("keywords", "space-separated document keywords").
-			Short('t').String()
-	docAuthor  = app.Flag("author", "document author").Short('a').String()
-	docCreator = app.Flag("creator", "document creator").
-			Default("ocrpdf").String()
+	docSize        = app.Flag("size", "document size").Short('s').Default("a4").String()
+	docTitle       = app.Flag("title", "document title").Short('t').String()
+	docSubject     = app.Flag("subject", "document subject").Short('j').String()
+	docKeywords    = app.Flag("keywords", "space-separated document keywords").Short('t').String()
+	docAuthor      = app.Flag("author", "document author").Short('a').String()
+	docCreator     = app.Flag("creator", "document creator").Default("ocrpdf").String()
 	docOrientation = app.Flag("orientation", "document orientation").
 			Default("auto").Short('r').Enum("auto", "portrait", "landscape")
-
-	compress = app.Flag("compress", "compress document").
+	docCompress = app.Flag("compress", "compress document").
 			Default("true").Short('c').Bool()
 
-	fontName = app.Flag("font-name", "text font").
-			Default("Arial").String()
+	fontName  = app.Flag("font-name", "text font").Default("Arial").String()
 	fontStyle = app.Flag("font-style", "font style, [B]old, [I]talic, [U]nderline").
-			PlaceHolder(" ").
-			Enum("B", "I", "U", "BI", "BU", "IU", "BIU")
-	fontSize = app.Flag("font-size", "OCR layer font size").
-			Default("10").Float()
+			PlaceHolder(" ").Enum("B", "I", "U", "BI", "BU", "IU", "BIU")
+	fontSize = app.Flag("font-size", "OCR layer font size").Default("10").Float()
+
 	textScaling = app.Flag("scaling", "Scale text to match word boundaries").
 			Default("match").Enum("off", "contain", "match")
 
-	force = app.Flag("force", "overwrite output file").Short('f').Bool()
-
-	imgContrast = app.Flag("contrast", "automatic contrast amount").
-			Default("0.5").Float()
-	imgFormat = app.Flag("format", "format to use when storing images in PDF").
+	imgContrast = app.Flag("contrast", "automatic contrast amount").Default("0.5").Float()
+	imgFormat   = app.Flag("format", "format to use when storing images in PDF").
 			Default("auto").Enum("auto", "jpg", "png")
-
-	input = app.Arg("input", "input image").Required().Strings()
 )
 
 func init() {
@@ -78,18 +69,13 @@ func main() {
 	doc.SetFont(*fontName, *fontStyle, *fontSize)
 	doc.SetTextScaling(TextScaling(*textScaling))
 	doc.SetTitle(*docTitle, true)
+	doc.SetSubject(*docSubject, true)
 	doc.SetKeywords(*docKeywords, true)
 	doc.SetAuthor(*docAuthor, true)
-	doc.SetCompression(*compress)
+	doc.SetCompression(*docCompress)
 	doc.SetOrientation(*docOrientation)
 
 	files := *input
-
-	if len(files) == 0 {
-		fmt.Printf("No file(s) specified!\n")
-		flag.Usage()
-		os.Exit(1)
-	}
 
 	// When only one file is specified, output to a PDF of the same name
 	outfn := *output
@@ -111,25 +97,27 @@ func main() {
 	outfile, err := os.OpenFile(outfn, openFlags, 0666)
 
 	if os.IsExist(err) {
-		fmt.Printf("Output file '%s' already exists. Use -force to overwrite.",
-			outfn)
+		fmt.Printf("Output file '%s' already exists. Use -force to overwrite.\n", outfn)
 		os.Exit(1)
 	} else if err != nil {
-		fmt.Printf("Couldn't create output file '%s': %s", outfn, err)
+		fmt.Printf("Couldn't create output file '%s': %s\n", outfn, err)
 		os.Exit(1)
 	}
 
 	// Iterate through each filename specified, adding a page for each
 	for i, fn := range files {
-		no := i + 1
-		logvf("[P%d] Reading '%s'...\n", no, fn)
+		pageno := i + 1
+
+		logvf("[P%d] Reading '%s'...\n", pageno, fn)
 		img := internal.NewImageFromFile(fn)
 		img = img.Adjust(float32(*imgContrast))
 		tess.SetImagePix(img.CPIX())
-		logvf("[P%d] Recognising...", no)
+
+		logvf("[P%d] Recognising...", pageno)
 		words := tess.Words()
 		logvf(" %d words found.\n", len(words))
-		logvf("[P%d] Adding page\n", no)
+
+		logvf("[P%d] Adding page\n", pageno)
 		err = doc.AddPage(fn, *img, words, *imgFormat)
 		if err != nil {
 			fmt.Println(err)
